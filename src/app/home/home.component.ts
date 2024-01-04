28ab5efccedd34b18c5d4fd26a4a3d1a917d6025
inso558 <parasbam3@gmail.com>
2024-01-04 19:04:48 +0545

    destination comp template added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { AboutComponent } from "../about/about.component";
+import { DestinationComponent } from "../destination/destination.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [FooterComponent, AboutComponent]
+    imports: [FooterComponent, AboutComponent, DestinationComponent]
 })
 export class HomeComponent {
 
